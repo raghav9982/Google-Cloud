d8cc4dc9a8fa6ea60759a426d807f8c589ca8433
raghav9982 <raghav9982@gmail.com>
2015-11-29 17:33:09 -0600

    Changes

--- tesre.php
+++
@@ -1,7 +0,0 @@
-$message = new Message();
-$message->setSender("sandesh2786@gmail.com");
-$message->addTo("sandeshguru.doddamane@mavs.uta.edu");
-$message->setSubject("Bucket Upload");
-$message->setTextBody("Example");
-$message->addAttachment($file,file_get_contents($filename['tmp_name']));
-$message->send();
